ab82ca470be5fd3da321d8825b696271915695ee
CustomSoftware <kcluff50@gmail.com>
2019-08-06 11:15:48 -0600

    Initial commit

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Administrator on 8/6/19.
+//  Copyright © 2019 CustomSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
